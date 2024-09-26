f608de08744b46b65925bfe791ae4debfbf46f01
guil <guil@DESKTOP-LPNQTOP>
2024-09-26 11:18:12 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeMedias/Media.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeMedias
+{
+    internal class Media
+    {
+        int qtde = 0;
+        public double Qtd()
+        {
+            Console.WriteLine("Digite a quantidade de notas: ");
+            qtde = int.Parse(Console.ReadLine());
+
+            double somaNotas = 0;
+
+            for (int i = 0; i < qtde; i++)
+            {
+                Console.WriteLine($"Digite a nota #{i+1}");
+                somaNotas += double.Parse(Console.ReadLine());
+            } ;
+
+            return somaNotas; 
+        }
+        public double CalculoMedia()
+        {
+            double notas = Qtd();
+            double media = notas / qtde;
+            Console.WriteLine($"Sua média é: {media}");
+
+            AprovadoReprovado(media);
+
+            return media;   
+        }
+
+        public void AprovadoReprovado(double media)
+        {
+            if (media <= 5) 
+            {
+                Console.WriteLine("Você está reprovado");
+            }
+            else
+            {
+                Console.WriteLine("Você está aprovado");  
+            }
+        }
+    }
+}
